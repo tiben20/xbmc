c19f35513e603df7ab9df57d661e07c9c48dc1c5
Benoit Plourde <curtis_20_b@hotmail.com>
2024-06-28 20:28:46 -0400

    Modified license

--- xbmc/cores/DSPlayer/Subtitles/subpic/DX11SubPic.h
+++ xbmc/cores/DSPlayer/Subtitles/subpic/DX11SubPic.h
@@ -1,8 +1,8 @@
 /*
  * (C) 2022-2024 see Authors.txt
- * This file is part of MPC-BE.
+ * This file is part of MPC-BE. But modified for Kodi
  *
- * MPC-BE is free software; you can redistribute it and/or modify
+ * Kodi is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
